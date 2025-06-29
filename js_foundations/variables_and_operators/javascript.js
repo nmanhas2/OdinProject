c35a066374b6a95a9d9f0aeb0a90eef83531bec5
nmanhas2 <manhasnubal@gmail.com>
2025-06-28 18:16:54 -0600

    Completed practice exercise for javascript

---
+++ js_foundations/variables_and_operators/javascript.js
@@ -0,0 +1,40 @@
+let name = "John";
+let surname = "Doe";
+
+console.log(name);
+console.log(surname);
+
+let age = 11;
+console.log(age);
+
+age = 54;
+
+console.log(age);
+
+const pi = 3.14;
+
+console.log(23 +97);
+
+console.log(1 + 2 + 3 +4 + 5 + 6);
+
+console.log((4 + 6 + 9) / 77);
+
+let a = 10;
+
+console.log(a);
+
+a = 5;
+
+console.log(a);
+
+let b = 7 * a;
+
+console.log(b);
+
+const max = 57;
+
+const actual = max - 13;
+
+const percentage = actual / max;
+
+console.log(percentage);
